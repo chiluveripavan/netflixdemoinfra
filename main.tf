faa1a2d60da3b91795622b9653d8f0b4fb46f4bf
chiluveripavan <pavanchiluveri74@gmail.com>
2025-03-26 19:39:27 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,7 +7,7 @@ count = 4
 ami = "ami-08b5b3a93ed654d19"
 instance_type = "t2.medium"
 key_name = "argocd"
-vpc_security_group_ids = ["sg-06785bc60f40dceda"]
+vpc_security_group_ids = ["sg-0923376d90ac594ff"]
 tags = {
 Name = var.instance_names[count.index]
 }
