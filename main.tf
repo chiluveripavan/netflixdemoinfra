36bd78d88f3afe3f2d3a115ba0ca07cafc490c04
chiluveripavan <pavanchiluveri74@gmail.com>
2025-03-26 20:03:18 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,7 +6,7 @@ resource "aws_instance" "one" {
 count = 4
 ami = "ami-08b5b3a93ed654d19"
 instance_type = "t2.medium"
-key_name = "argocd"
+key_name = "my-key-pair"
 vpc_security_group_ids = ["sg-0923376d90ac594ff"]
 tags = {
 Name = var.instance_names[count.index]
