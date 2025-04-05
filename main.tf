a438f46416758c23166d2e33d6ddc678c6ae5cf8
chiluveripavan <pavanchiluveri74@gmail.com>
2025-04-05 10:18:54 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ region = "us-east-2"
 
 resource "aws_instance" "one" {
 count = 4
-ami = "ami-00a929b66ed6e0de6"
+ami = "ami-0100e595e1cc1ff7f"
 instance_type = "t2.medium"
 key_name = "pavankey"
 vpc_security_group_ids = ["sg-09fedc86846dce45d"]
